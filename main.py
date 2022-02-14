460d277a8fcbd89ffb8afcb575fe68ecc4680738
shaslhofer <Ch.li.lu.se>
2022-02-14 19:27:06 +0100

    added mapped directory to gitignore

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from qiskit import QuantumCircuit, transpile
 from qiskit.test.mock.backends import FakeBrooklyn
 
-path = './emtec21/benchmarks/original/circuit.qasm'
+path = './original/adder-3.qasm'
 qc = QuantumCircuit.from_qasm_file(path=path)
 qc_transpiled = transpile(qc, backend=FakeBrooklyn())
-filename = './emtec21/benchmarks/mapped/circuit.qasm'
+filename = './mapped/adder-3.qasm'
 qc_transpiled.qasm(filename=filename)
