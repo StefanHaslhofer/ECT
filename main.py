cf7dc935193897fd727d049cdf6d90ff056dd4e3
StefanHaslhofer <haslhofer.stefan@gmail.com>
2022-02-21 17:19:25 +0100

    main.py example

--- main.py
+++ main.py
@@ -1,30 +1,54 @@
-from qiskit import QuantumCircuit, transpile
-from qiskit.dagcircuit import DAGCircuit
-from qiskit.test.mock.backends import FakeBrooklyn
+from qiskit import QuantumCircuit
 from qiskit.converters import circuit_to_dag
+from qiskit.transpiler import CouplingMap
+
+# coupling of a brooklyn device as a list representation
+coupling = [[0, 1], [1, 2], [2, 3]]
+
 
 # based on algorithms described in https://pypi.org/project/quantum-qubit-mapping/
 def q_transpile(c):
-    qc_transpiled = QuantumCircuit(c.qregs[0].size, c.cregs[0].size)
+    qc_transpiled = QuantumCircuit(len(c.qubits), len(c.qubits))
 
+    front_layer = ""  # TODO search for a way to get the first independent CNOT gates in dag
+    coupling_map = CouplingMap(couplinglist=coupling)
+
+    mapping = initiate_mapping(ghz)
+    # initiate the circuits directed acyclic graph (dag)
+    dag = circuit_to_dag(ghz)
+
+    # mapping = sabre_swap(,mapping,,dag,coupling_map)
     return qc_transpiled
 
 
 def sabre_swap(front_layer, mapping, dm, dag, coupling_graph):
-    return ""
+    return mapping
+
+
+# distances between two physical bits
+def get_distance_matrix(c):
+    dm = []
+    for i in range(len(c.qubits)):
+        col = []
+        for j in range(len(c.qubits)):
+            col.append(0)
+        dm.append(col)
+
+    return dm
+
 
 # map => <physical Q-bit>:<logical Q-bit>
 # 1 to 1 initial mapping
-def initiateMapping(c):
+def initiate_mapping(c):
     mapping = {}
-    for i in range(c.qregs[0].size):
+    for i in range(len(c.qubits)):
         mapping['Q_' + str(i)] = 'q_' + str(i)
 
     return mapping
 
 
-path = './original/ghz_state-5.qasm'
-qc = QuantumCircuit.from_qasm_file(path=path)
+#####path = './original/ghz_state-5.qasm'
+#####qc = QuantumCircuit.from_qasm_file(path=path)
 
 ghz = QuantumCircuit(3, 3)
 ghz.h(0)
@@ -33,20 +57,16 @@ ghz.barrier()
 ghz.measure(range(3), range(3))
 print(ghz.draw(output='text'))
 
-mapping = initiateMapping(ghz)
-# initiate the circuits directed acyclic graph (dag)
-dag = circuit_to_dag(ghz)
-
 qc_transpiled = q_transpile(ghz)
 print(qc_transpiled.draw(output='text'))
 
-# print(qc.draw(output='text'))
-#
-# circ = QuantumCircuit(3)
-# circ.ccx(0, 1, 2)
-#
-# print(circ.draw(output='text'))
-#
-# qc_transpiled = transpile(qc, backend=FakeBrooklyn())
-# filename = './mapped/ghz_state-5.qasm'
-# qc_transpiled.qasm(filename=filename)
+##### print(qc.draw(output='text'))
+#####
+##### circ = QuantumCircuit(3)
+##### circ.ccx(0, 1, 2)
+#####
+##### print(circ.draw(output='text'))
+#####
+##### qc_transpiled = transpile(qc, backend=FakeBrooklyn())
+##### filename = './mapped/ghz_state-5.qasm'
+##### qc_transpiled.qasm(filename=filename)
